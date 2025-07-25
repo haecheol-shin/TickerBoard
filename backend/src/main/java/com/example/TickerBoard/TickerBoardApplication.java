bbd3ebb1d22a9aeae770a3fa7ec1c25f76114a8d
Haecheol Shin <dev.hc.shin@gmail.com>
2025-07-25 15:52:10 +0900

    first commit

---
+++ backend/src/main/java/com/example/TickerBoard/TickerBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.TickerBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickerBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickerBoardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/TickerBoard/TickerBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TickerBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickerBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
