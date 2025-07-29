049e768037853c0efec675d64e39bc67b01ad977
Haecheol Shin <dev.hc.shin@gmail.com>
2025-07-29 14:55:58 +0900

    Test commit for CI/CD frontend

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -14,8 +14,7 @@ const count = ref(0)
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      🎉 배포 테스트 성공!
     </p>
   </div>
 
